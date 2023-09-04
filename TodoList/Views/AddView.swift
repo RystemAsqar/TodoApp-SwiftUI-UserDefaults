f68f9f8242416bc837cdb4f95df99c1f3c5f1958
Rystem Asqar <bornqazaqi@gmail.com>
2023-09-05 05:55:00 +0600

    following a MVVM pattern, create models and ViewModels, and connect there with View

--- TodoList/Views/AddView.swift
+++ TodoList/Views/AddView.swift
@@ -8,10 +8,15 @@
 import SwiftUI
 
 struct AddView: View {
+    
+    @Environment(\.presentationMode) var presentationMode
+    @EnvironmentObject var listViewModel: ListViewModel
+    @State var textFieldText: String = ""
+    
+    @State var alertTitle: String = ""
+    @State var showAlert: Bool = false
+    
     var body: some View {
-        
-        @State var textFieldText: String = ""
-        
         ScrollView {
             VStack {
                 TextField("type something here...", text: $textFieldText)
@@ -21,7 +26,7 @@ struct AddView: View {
                     .cornerRadius(10)
                 
                 Button {
-                    
+                    saveButtonPressed()
                 } label: {
                     Text("Save")
                         .foregroundColor(.white)
@@ -35,6 +40,27 @@ struct AddView: View {
             }.padding(14)
         }
         .navigationTitle("Add an Item🖊️")
+        .alert(isPresented: $showAlert, content: getAlert)
+    }
+    
+    func saveButtonPressed() {
+        if textIsAppropriate() {
+            listViewModel.addItem(title: textFieldText)
+            presentationMode.wrappedValue.dismiss()
+        }
+    }
+    
+    func textIsAppropriate() -> Bool {
+        if textFieldText.count < 4 {
+            alertTitle = "Your new todo task's item must be at least 4 characters!🥷🏽"
+            showAlert.toggle()
+            return false
+        }
+        return true
+    }
+    
+    func getAlert() -> Alert {
+        return Alert(title: Text(alertTitle))
     }
 }
 
@@ -43,5 +69,6 @@ struct AddView_Previews: PreviewProvider {
         NavigationView {
             AddView()
         }
+        .environmentObject(ListViewModel())
     }
 }
--- TodoList/Views/ListView.swift
+++ TodoList/Views/ListView.swift
@@ -9,20 +9,24 @@ import SwiftUI
 
 struct ListView: View {
     
-    @State var items: [String] = [
-        "This is the first item!",
-        "This is the second",
-        "This is the third!"
-    ]
+    @EnvironmentObject var listViewModel: ListViewModel
+    
     var body: some View {
         List {
-            ForEach(items, id: \.self) { item in
-                ListRowView(title: item)
+            ForEach(listViewModel.items) { item in
+                ListRowView(item: item)
+                    .onTapGesture {
+                        withAnimation(.linear) {
+                            listViewModel.updateItem(item: item)
+                        }
+                    }
             }
+            .onDelete(perform: listViewModel.deleteItem)
+            .onMove(perform: listViewModel.moveItem)
         }
         .listStyle(PlainListStyle())
-        .navigationTitle("ToDo List")
-        .navigationBarItems(leading: EditButton(), trailing: NavigationLink("add", destination: AddView()))
+        .navigationTitle("ToDo List📃")
+        .navigationBarItems(leading: EditButton(), trailing: NavigationLink("Add", destination: AddView()))
     }
 }
 
@@ -31,5 +35,6 @@ struct ListView_Previews: PreviewProvider {
         NavigationView {
             ListView()
         }
+        .environmentObject(ListViewModel())
     }
 }
